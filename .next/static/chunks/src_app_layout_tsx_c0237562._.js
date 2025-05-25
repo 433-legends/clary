9159851277ca2a90ec954efc3e4cc0497ef22362
sitanshu10 <sitanshu1097@gmail.com>
2025-05-25 14:26:46 -0700

    Fix force p changes (#3)
    * Add python set up
    
    * UI updates
    
    * added cursor pointer wherever necessary
    
    ---------
    
    Co-authored-by: Apple <apple@Apples-MacBook-Pro-3.local>
    Co-authored-by: Calypso4597 <sanmid.anavkar@gmail.com>

--- .next/static/chunks/src_app_layout_tsx_c0237562._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__9a168d68._.css",
-  "static/chunks/src_aca25184._.js",
-  "static/chunks/node_modules_cb6123f3._.js"
-],
-    source: "dynamic"
-});
